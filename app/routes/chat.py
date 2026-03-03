cb81038bcc4daf317335364192611254999a14ff
dibya-22 <dibyalochanpattnayak045@gmail.com>
2026-03-03 11:00:04 +0530

    feat(app): implement chat session controller with mode switching

---
+++ app/routes/chat.py
@@ -0,0 +1,52 @@
+from chains.chat import chat, get_graph
+from memory.user_memory import add_memory
+from voice.stt import listen
+from voice.tts import speak
+
+def start_chat(user: dict):
+    user_id = user["user_id"]
+    print("Which Mode Do You Prefer?")
+    choice = input("1. Text\n2. Voice\nChoose: ").lower()
+
+    mode = "text" #? default
+    match choice:
+        case "1" | "text":
+            mode = "text"
+        case "2" | "voice":
+            mode = "voice"
+        case _:
+            print("Invalid Mode. Setting mode to Text")
+            mode = "text"
+    with get_graph() as checkpointer:
+        while True:
+            if mode == "voice":
+                print(">", end=" ", flush=True)
+                user_input = listen()
+                print(user_input)
+            else:
+                user_input = input("> ")
+
+            if not user_input:
+                if mode == "voice":
+                    speak("Invalid Input! Please Enter Again")
+                print("Invalid Input! Please Enter Again")
+                continue
+
+            response, new_mode, should_terminate = chat(user_input, user_id, "gf", checkpointer)
+
+            if mode == "voice":
+                speak(response)
+                print(f"🤖: {response}\n")
+            else:
+                print(f"🤖: {response}\n")
+
+            add_memory(user_input, response, user_id)
+
+            if should_terminate:
+                if mode == "voice":
+                    speak("Bye! Talk soon")
+                print("🤖: Bye! Talk soon 💕")
+
+            if new_mode and new_mode != mode:
+                mode = new_mode
+                print(f"[Switched to {mode} mode]")
